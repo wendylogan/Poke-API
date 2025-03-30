44252b5046242c4e35f350bf924fbd5b262f766f
wendyologan <133799226+wendyologan@users.noreply.github.com>
2025-03-30 15:14:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function pokeInfo(){
 
     // get the name of the pokemon the user selected and output it
     let pokemon = document.querySelector("#pokeMenu").value;
-    document.querySelector("#name").innerHTML = "Name: " + pokemon;
+    document.querySelector("#name").innerHTML = "Name: " + String(pokemon).charAt(0).toUpperCase() + String(pokemon).slice(1);
     
     // create the request
     const request = new XMLHttpRequest();
